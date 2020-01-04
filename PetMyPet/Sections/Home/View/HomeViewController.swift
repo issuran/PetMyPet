64b7e6b2c91f5aa241434a8beef0610197797a55
Tiago Oliveira <tiago_fernandes89@hotmail.com>
2020-01-03 22:49:04 -0300

    Adding table view home

--- PetMyPet/Sections/Home/View/HomeViewController.swift
+++ PetMyPet/Sections/Home/View/HomeViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class HomeViewController: UIViewController {
     
     var viewModel: HomeViewModel?
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,15 +26,4 @@ class HomeViewController: UIViewController {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
