8dfa3a188d7b21293f3cbfd1933d99e1ebbf948c
Tiago Oliveira <tiago_fernandes89@hotmail.com>
2020-02-04 21:54:19 -0300

    Reset card after dismiss

--- PetMyPet/Sections/Home/View/HomeViewController.swift
+++ PetMyPet/Sections/Home/View/HomeViewController.swift
@@ -13,30 +13,49 @@ class HomeViewController: UIViewController {
     var viewModel: HomeViewModel?
     @IBOutlet weak var swipeableCardView: SwipeableCardView!
     
+    @IBOutlet weak var resetButton: PMPButton!
+    
     @IBAction func panView(_ sender: UIPanGestureRecognizer) {
         let point = sender.translation(in: self.view)
+        let card = sender.view!
+        let xFromCenter = card.center.x - view.center.x
         
-        if let card = sender.view {
-            let xFromCenter = card.center.x - view.center.x
-            
-            card.center = CGPoint(x: card.center.x + point.x,
-                                  y: card.center.y + point.y)
-            
-            if xFromCenter > 0 {
-                swipeableCardView.feedbackImageView.image = #imageLiteral(resourceName: "Happy")
-                swipeableCardView.feedbackImageView.tintColor = .green
-            } else {
-                swipeableCardView.feedbackImageView.image = #imageLiteral(resourceName: "Sad")
-                swipeableCardView.feedbackImageView.tintColor = .red
-            }
-            
-            sender.setTranslation(CGPoint(x: 0, y: 0), in: card)
+        card.center = CGPoint(x: card.center.x + point.x,
+                              y: card.center.y + point.y)
+        
+        if xFromCenter > 0 {
+            swipeableCardView.feedbackImageView.image = #imageLiteral(resourceName: "Happy")
+            swipeableCardView.feedbackImageView.tintColor = .green
+        } else {
+            swipeableCardView.feedbackImageView.image = #imageLiteral(resourceName: "Sad")
+            swipeableCardView.feedbackImageView.tintColor = .red
         }
         
+        swipeableCardView.feedbackImageView.alpha = abs(xFromCenter) / view.center.x
+        
+        sender.setTranslation(CGPoint(x: 0, y: 0), in: card)
+        
         if sender.state == .ended {
-            UIView.animate(withDuration: 0.2) {
-                sender.view?.center = self.view.center
+            
+            if card.center.x < 40 {
+                UIView.animate(withDuration: 0.3) {
+                    card.center = CGPoint(x: card.center.x - 200, y: card.center.y + 75)
+                    card.alpha = 0
+                }
+                return
+            } else if card.center.x > (view.frame.width - 40) {
+                UIView.animate(withDuration: 0.3) {
+                    card.center = CGPoint(x: card.center.x + 200, y: card.center.y + 75)
+                    card.alpha = 0
+                }
+                return
             }
+            
+//            UIView.animate(withDuration: 0.2) {
+//                sender.view?.center = self.view.center
+//                self.swipeableCardView.feedbackImageView.alpha = 0
+//            }
+            resetCards()
         }
     }
     
@@ -52,4 +71,17 @@ class HomeViewController: UIViewController {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
+    
+    @IBAction func resetAction(_ sender: Any) {
+        resetCards()
+    }
+    
+    func resetCards() {
+        UIView.animate(withDuration: 0.2) {
+            self.swipeableCardView.center = self.view.center
+            self.swipeableCardView.feedbackImageView.alpha = 0
+            self.swipeableCardView.alpha = 1
+        }
+    }
+    
 }
