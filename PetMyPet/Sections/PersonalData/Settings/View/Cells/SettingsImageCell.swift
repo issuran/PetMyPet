66bfd8014e5f128faf9b7d95d346891ce0eeb95a
Tiago Oliveira <tiago_fernandes89@hotmail.com>
2020-02-20 23:51:37 -0300

    Images table view cell

---
+++ PetMyPet/Sections/PersonalData/Settings/View/Cells/SettingsImageCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsImageCell.swift
+//  PetMyPet
+//
+//  Created by Tiago Oliveira on 20/02/20.
+//  Copyright © 2020 Tiago Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class SettingsImageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
